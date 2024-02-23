a934ae8f83b88c7a5b000036a59f878548791dc7
unknown <macadrian20@gmail.com>
2024-02-22 17:20:57 -0800

    All requirements have been fulfilled except for the point keeping and the winner anouncement.

--- RPS.js
+++ RPS.js
@@ -7,26 +7,61 @@
 // 6. use input case sensitive
 // 6. playGame() -- five rounds. Keep score. Report a winner and loser.
 
-const propmt = require("prompt-sync")();
+const prompt = require("prompt-sync")(); 
 
 // getComputerChoice function
 const getComputerChoice = () =>{
     const options = ['Rock','Paper','Scissors'];
     var rand = options[Math.random() * options.length>>0];
-    console.log(rand);
-    return(rand);
+    return(rand.toLowerCase());
+}
+
+// playerSelection function
+
+const playerSelection = () =>{
+    var userSelect = prompt("Choose rock, paper, or scissors: ");
+    return(userSelect.toLowerCase());
+}
+
+// Single round version (Game flow logic)
+
+const singleRound = () =>{
+    let PS = playerSelection();
+    let GC = getComputerChoice();
+    if(PS == GC){
+        console.log("It was a tie!");
+  
+  //win conditions first
+    }else if(GC == "rock" && PS == "paper"){ // I acknowledge theres certainly a better way to do this but I'm tired so I'm just gonna do it this way for now.
+        console.log("You Won! Your opponent chose rock.");
+    }else if(GC == "paper" && PS == "scissors"){
+        console.log("You Won! Your opponent chose paper.");
+    }else if(GC == "scissors" && PS == "rock"){
+        console.log("You won! Your opponent chose scissors.");
+    }else if(GC == "paper" && PS == "rock"){
+        console.log("You Lost! Your opponent chose paper.");
+    }else if(GC == "scissors" && PS == "paper"){
+        console.log("You Lost! Your opponent chose scissors.");
+    }else{
+        console.log("You Lost! Your opponent chose rock.");
+    }
+    
 }
 
 
 
+// best of five rounds
+
+const fiveRounds = () =>{
+        const isReady = prompt('Welcome to "Rock, Paper, Scissors". Are you ready to play?(y/n): ');
+        if (isReady.toLowerCase() == "y"){
+            for (let i = 0; i < 5; i++){
+                singleRound();
+            }
+    }
+}
 
 
 
-
-
-
-
-
-
-
-
+// run the five rounds of RPS.
+fiveRounds()
